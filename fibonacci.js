b8e35a129e16cfdf807507efdf5c5d2042514721
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-09-30 20:42:08 +0400

    add recrusive fib function

--- fibonacci.js
+++ fibonacci.js
@@ -14,4 +14,24 @@ const fibs = (num) => {
   return fibsArray;
 }
 
-fibs(10);
+// fibs(10);
+
+const fibsRec = (num) => { 
+  if (num <= 0) {
+    return [];
+  }
+  if (num === 1) {
+    return [0];
+  }
+  if (num === 2) {
+    return [0, 1];
+  }
+  const fibsArray = fibsRec(num - 1);  
+  const rest = fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3];
+    
+  return [...fibsArray, rest];
+  
+}
+
+console.log(fibsRec(8));
+
